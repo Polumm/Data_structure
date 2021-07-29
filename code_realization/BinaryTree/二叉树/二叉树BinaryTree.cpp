a025e77a62fb7579f4b5a116aeee8c1d847a6aa9
Polumm <songchujiashenzhen@163.com>
2021-07-29 22:23:41 +0800

    二叉树非递归遍历的算法(线索化实现)

--- code_realization/BinaryTree/二叉树/二叉树BinaryTree.cpp
+++ code_realization/BinaryTree/二叉树/二叉树BinaryTree.cpp
@@ -87,6 +87,26 @@ void makeTread(BiTree& T)
 		pre->rtag = 1;
 	}
 }
+//找到以当前节点为根的子树中，第一个被中序遍历的结点
+BiTNode* Firstnode(BiTNode* p)
+{
+	if (p != NULL)
+	{
+		while (p->ltag == 0)	p = p->lchild;//一直找子树中的左孩子
+		return p;
+	}
+}
+//在中序遍历中找到p的后继节点
+BiTNode* Nextnode(BiTNode* p)
+{
+	if (p->rtag == 0)	return Firstnode(p->rchild);
+	else return p->rchild;//rtag==1直接返回
+}
+void TreadInOrder(BiTNode* T)
+{
+	for (BiTNode* p = Firstnode(T); p != NULL; p = Nextnode(p))
+		vist(p);
+}
 int main()
 {
 	BiTree root = NULL;
@@ -97,5 +117,7 @@ int main()
 	BiTreeIncrease(root->rchild->rchild, 17);
 	BiTreeIncrease(root->rchild->lchild->lchild, 13);
 	InOrder(root);
-	makeTread(root);
+	cout << "//===========================" << endl;
+	makeTread(root);//线索化实现的非递归遍历算法
+	TreadInOrder(root);
 }
